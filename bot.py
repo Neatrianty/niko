0b83d85febf68c32822c1b0edf2145294f6f1dcd
Neatrianty <mardomyan102@gmail.com>
2018-03-18 06:20:16 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,40 @@
+import discord
+import asyncio
+import random
+import pickle
+import os
 
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
+ 
+	@client.event
+	async def on_message(message):
+		if message.content.upper().startswith('N!NIKO'):
+			await client.send_message(message.channel, 'Yeah?')
+		elif message.content.startswith('n!pick'):
+			flip = random.choice(['Of course I would pick Crimped', 'Of course I would pick Indomie', 'Of course I would pick Myth', 'Of course I would pick Tuan', 'Of course I would pick Tuna', 'Of couse I would pick Scoot', 'Of course I would pick Creepy', 'Of course I would pick myself'])
+			await client.send_message(message.channel, flip)
+		elif message.content.startswith('n!skip'):
+			await client.send_message(message.channel, 'Why skip ;-;')
+		elif message.content.startswith('!pancake'):
+			await client.send_message(message.channel,  ':3')
+		elif message.content.startswith('!wakeup'):
+			await client.send_message(message.channel, 'No Im going back to sleep')	
+		elif message.content.startswith('!meow'):
+			await client.send_message(message.channel, 'Meow :3')
+		elif message.content.startswith('n!help'):
+			await client.send_message(message.channel, '**COMMANDS** n!niko, n!pick, n!skip, !pancake, !wakeup, !meow, n!help, n!hi')
+		elif message.content.upper().startswith('N!HI'):
+			userID = message.author.id
+			await client.send_message(message.channel, "Hi, <@%s>" % (userID))
+	
+
+client.run('BOT_TOKEN')
+ 
  
