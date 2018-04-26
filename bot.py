1a30573f6a47dead289487381b3c66ee94b9fb16
Neatrianty <mardomyan102@gmail.com>
2018-04-26 13:00:02 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,8 +17,8 @@ async def on_ready():
 	async def on_message(message):
 		if message.content.upper().startswith('N!NIKO'):
 			await client.send_message(message.channel, 'Yeah?')
-		elif message.content.startswith('n!pick'):
-			flip = random.choice(['Of course I would pick Crimped', 'Of course I would pick Indomie', 'Of course I would pick Myth', 'Of course I would pick Tuan', 'Of course I would pick Tuna', 'Of couse I would pick Scoot', 'Of course I would pick Creepy', 'Of course I would pick myself'])
+		elif message.content.upper().startswith('N!PICK'):
+			flip = random.choice(['Of course I would pick Crimped', 'Of course I would pick Indomie', 'Of course I would pick Myth', 'Of course I would pick Tuan', 'Of course I would pick fckboi', 'Of course I would pick Tuna', 'Of couse I would pick Scoot', 'Of course I would pick Creepy', 'Of course I would pick myself'])
 			await client.send_message(message.channel, flip)
 		elif message.content.startswith('n!skip'):
 			await client.send_message(message.channel, 'Why skip ;-;')
@@ -33,8 +33,15 @@ async def on_ready():
 		elif message.content.upper().startswith('N!HI'):
 			userID = message.author.id
 			await client.send_message(message.channel, "Hi, <@%s>" % (userID))
-	
+		elif message.content.upper().startswith('N!QUOTE'):
+			b = random.randint(0,51)
 
+			a = open("quotes.txt", "r")
+			for  x, line in enumerate(a):
+				if x == b:
+					await client.send_message(message.channel, line)
+			a.close()
+
+		
 client.run('BOT_TOKEN')
- 
- 
+
